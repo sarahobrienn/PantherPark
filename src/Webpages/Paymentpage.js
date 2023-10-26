6cb39dcdc16789a86aa02a621cacb456fc4237cd
sarahmosouthernco <smobrien@southernco.com>
2023-10-26 19:05:54 -0400

    design changes

--- src/Webpages/Paymentpage.js
+++ src/Webpages/Paymentpage.js
@@ -1,13 +1,85 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import PaymentContext from '../PaymentContext';
 
 function PaymentPage() {
+  const [cardNumber, setCardNumber] = useState('');
+  const [cardName, setCardName] = useState('');
+  const [cvv, setCvv] = useState('');
+  const [expDate, setExpDate] = useState('');
+  const [zipCode, setZipCode] = useState('');
+
+  const { setPaymentData } = useContext(PaymentContext);
+  const navigate = useNavigate();
+
+  const handlePayment = () => {
+    setPaymentData({
+      cardNumber,
+      cardName,
+      cvv,
+      expDate,
+      zipCode
+    });
+
+    navigate('/confirmation');
+  };
+
   return (
-    <div>
-      <h1>Welcome to the Payment Page</h1>
-      <Link to="/login">
-        <button>Back to Login Page</button>
-      </Link>
+    <div className="container">
+      <header>
+        <img src="/gsu_logo.png" alt="Georgia State University Logo" width="100" />
+        <h1>Panther Park</h1>
+      </header>
+      <h2 className="page-title">Payment Information</h2>
+      <div className="payment-form">
+        <input 
+          type="text" 
+          placeholder="Credit Card Number" 
+          className="input-field" 
+          value={cardNumber} 
+          onChange={(e) => setCardNumber(e.target.value)}
+        />
+        <input 
+          type="text" 
+          placeholder="Cardholder Name" 
+          className="input-field" 
+          value={cardName} 
+          onChange={(e) => setCardName(e.target.value)}
+        />
+        <input 
+          type="password" 
+          placeholder="CVV" 
+          className="input-field" 
+          value={cvv} 
+          onChange={(e) => setCvv(e.target.value)}
+        />
+        <input 
+          type="text" 
+          placeholder="Expiration Date (MM/YY)" 
+          className="input-field" 
+          value={expDate} 
+          onChange={(e) => setExpDate(e.target.value)}
+        />
+        <input 
+          type="text" 
+          placeholder="Zip Code" 
+          className="input-field" 
+          value={zipCode} 
+          onChange={(e) => setZipCode(e.target.value)}
+        />
+        <button className="button" onClick={handlePayment}>Make Payment</button>
+      </div>
+      <div className="content">
+        <Link to="/user">
+          <button className="button">Back to User Page</button>
+        </Link>
+        <Link to="/">
+          <button className="button">Back to Home</button>
+        </Link>
+      </div>
+      <footer>
+        {/* Any footer content if needed */}
+      </footer>
     </div>
   );
 }
