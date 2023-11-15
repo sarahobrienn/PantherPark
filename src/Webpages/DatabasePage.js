37cc156a1a6e7946fa82e322903f612a7cabd858
hash0215 <ha.serena.h@gmail.com>
2023-11-15 13:08:16 -0500

    database edits

--- src/Webpages/DatabasePage.js
+++ src/Webpages/DatabasePage.js
@@ -1,28 +1,15 @@
+// DatabasePage.js
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import './DatabasePage.css';
 
 function DatabasePage() {
-  const navigate = useNavigate();
-
-  const handleGoBack = () => {
-    navigate(-1); // Go back one step in history
-  };
-
   return (
     <div className="database-container">
-      <header>
-        <h1>Database Page</h1>
-      </header>
-      <div className="content">
-        <p>Content of the Database Page goes here.</p>
-        <button className="link-button" onClick={handleGoBack}>
-          Go Back
-        </button>
-      </div>
+      <h2 className="page-title">Database Page</h2>
+      {/* Add your database-related content here */}
     </div>
   );
 }
 
 export default DatabasePage;
-
+//
